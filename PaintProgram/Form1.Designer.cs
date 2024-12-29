1571105382719b12e6c0adfe4aca6c392cf4993c
dimowsl <127833200+dimowsl@users.noreply.github.com>
2024-12-29 20:36:45 +0200

    Add files via upload

---
+++ PaintProgram/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace PaintProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnPurple = new System.Windows.Forms.Button();
+            this.btnSmall = new System.Windows.Forms.Button();
+            this.btnErase = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnMedium = new System.Windows.Forms.Button();
+            this.btnLarge = new System.Windows.Forms.Button();
+            this.btnBlack = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.btnBucket = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPurple
+            // 
+            this.btnPurple.BackColor = System.Drawing.Color.Purple;
+            this.btnPurple.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnPurple.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPurple.ForeColor = System.Drawing.Color.White;
+            this.btnPurple.Location = new System.Drawing.Point(435, 6);
+            this.btnPurple.Name = "btnPurple";
+            this.btnPurple.Size = new System.Drawing.Size(80, 39);
+            this.btnPurple.TabIndex = 5;
+            this.btnPurple.Text = "PURPLE";
+            this.btnPurple.UseVisualStyleBackColor = false;
+            this.btnPurple.Click += new System.EventHandler(this.btnPurple_Click);
+            // 
+            // btnSmall
+            // 
+            this.btnSmall.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSmall.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSmall.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSmall.ForeColor = System.Drawing.Color.White;
+            this.btnSmall.Location = new System.Drawing.Point(583, 6);
+            this.btnSmall.Name = "btnSmall";
+            this.btnSmall.Size = new System.Drawing.Size(53, 39);
+            this.btnSmall.TabIndex = 6;
+            this.btnSmall.Text = "Small";
+            this.btnSmall.UseVisualStyleBackColor = false;
+            this.btnSmall.Click += new System.EventHandler(this.btnSmall_Click);
+            // 
+            // btnErase
+            // 
+            this.btnErase.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnErase.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnErase.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnErase.ForeColor = System.Drawing.Color.White;
+            this.btnErase.Location = new System.Drawing.Point(95, 51);
+            this.btnErase.Name = "btnErase";
+            this.btnErase.Size = new System.Drawing.Size(80, 63);
+            this.btnErase.TabIndex = 10;
+            this.btnErase.Text = "Eraser";
+            this.btnErase.UseVisualStyleBackColor = false;
+            this.btnErase.Click += new System.EventHandler(this.btnErase_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.BackColor = System.Drawing.Color.Lime;
+            this.btnGreen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnGreen.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGreen.Location = new System.Drawing.Point(95, 6);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(80, 39);
+            this.btnGreen.TabIndex = 1;
+            this.btnGreen.Text = "GREEN";
+            this.btnGreen.UseVisualStyleBackColor = false;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.Blue;
+            this.btnBlue.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBlue.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBlue.ForeColor = System.Drawing.Color.White;
+            this.btnBlue.Location = new System.Drawing.Point(180, 6);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(80, 39);
+            this.btnBlue.TabIndex = 2;
+            this.btnBlue.Text = "BLUE";
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.BackColor = System.Drawing.Color.Yellow;
+            this.btnYellow.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnYellow.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnYellow.Location = new System.Drawing.Point(350, 6);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(80, 39);
+            this.btnYellow.TabIndex = 4;
+            this.btnYellow.Text = "YELLOW";
+            this.btnYellow.UseVisualStyleBackColor = false;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRed.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRed.Location = new System.Drawing.Point(12, 6);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(80, 39);
+            this.btnRed.TabIndex = 0;
+            this.btnRed.Text = "RED";
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnMedium
+            // 
+            this.btnMedium.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnMedium.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMedium.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMedium.ForeColor = System.Drawing.Color.White;
+            this.btnMedium.Location = new System.Drawing.Point(642, 6);
+            this.btnMedium.Name = "btnMedium";
+            this.btnMedium.Size = new System.Drawing.Size(69, 39);
+            this.btnMedium.TabIndex = 7;
+            this.btnMedium.Text = "Medium";
+            this.btnMedium.UseVisualStyleBackColor = false;
+            this.btnMedium.Click += new System.EventHandler(this.btnMedium_Click);
+            // 
+            // btnLarge
+            // 
+            this.btnLarge.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnLarge.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLarge.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLarge.ForeColor = System.Drawing.Color.White;
+            this.btnLarge.Location = new System.Drawing.Point(717, 6);
+            this.btnLarge.Name = "btnLarge";
+            this.btnLarge.Size = new System.Drawing.Size(58, 39);
+            this.btnLarge.TabIndex = 8;
+            this.btnLarge.Text = "Large";
+            this.btnLarge.UseVisualStyleBackColor = false;
+            this.btnLarge.Click += new System.EventHandler(this.btnLarge_Click);
+            // 
+            // btnBlack
+            // 
+            this.btnBlack.BackColor = System.Drawing.Color.Black;
+            this.btnBlack.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBlack.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBlack.ForeColor = System.Drawing.Color.White;
+            this.btnBlack.Location = new System.Drawing.Point(265, 6);
+            this.btnBlack.Name = "btnBlack";
+            this.btnBlack.Size = new System.Drawing.Size(80, 39);
+            this.btnBlack.TabIndex = 3;
+            this.btnBlack.Text = "BLACK";
+            this.btnBlack.UseVisualStyleBackColor = false;
+            this.btnBlack.Click += new System.EventHandler(this.btnBlack_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDelete.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(12, 51);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(80, 63);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSaveAs.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSaveAs.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveAs.ForeColor = System.Drawing.Color.White;
+            this.btnSaveAs.Location = new System.Drawing.Point(181, 51);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(80, 63);
+            this.btnSaveAs.TabIndex = 11;
+            this.btnSaveAs.Text = "Save As";
+            this.btnSaveAs.UseVisualStyleBackColor = false;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // btnBucket
+            // 
+            this.btnBucket.BackColor = System.Drawing.Color.White;
+            this.btnBucket.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBucket.BackgroundImage")));
+            this.btnBucket.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBucket.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBucket.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBucket.ForeColor = System.Drawing.Color.White;
+            this.btnBucket.Location = new System.Drawing.Point(267, 51);
+            this.btnBucket.Name = "btnBucket";
+            this.btnBucket.Size = new System.Drawing.Size(68, 63);
+            this.btnBucket.TabIndex = 12;
+            this.btnBucket.UseVisualStyleBackColor = false;
+            this.btnBucket.Click += new System.EventHandler(this.btnBucket_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnBucket);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnErase);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnLarge);
+            this.Controls.Add(this.btnMedium);
+            this.Controls.Add(this.btnSmall);
+            this.Controls.Add(this.btnPurple);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnBlack);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnRed);
+            this.Name = "Form1";
+            this.Text = "Simple Paint Program";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPurple;
+        private System.Windows.Forms.Button btnSmall;
+        private System.Windows.Forms.Button btnErase;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnMedium;
+        private System.Windows.Forms.Button btnLarge;
+        private System.Windows.Forms.Button btnBlack;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Button btnBucket;
+    }
+}
+
---
+++ PaintProgram/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PaintProgram
+{
+    public partial class Form1 : Form
+    {
+        private Color selectedColor = Color.Black;
+        private bool isDrawing = true;
+        private int brushSize = 2;
+        private bool isMouseDown = false;
+        private Point lastPoint = Point.Empty;
+        private Bitmap canvas;
+        private Bitmap resizedCanvas; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseDown += Form1_MouseDown;
+            this.MouseMove += Form1_MouseMove;
+            this.MouseUp += Form1_MouseUp;
+            canvas = new Bitmap(this.Width, this.Height);
+            this.SetStyle(ControlStyles.DoubleBuffer | ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
+            this.UpdateStyles();
+            canvas = new Bitmap(this.Width, this.Height);
+            this.Resize += Form1_Resize;
+            canvas = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            canvas = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            resizedCanvas = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            using (Graphics g = Graphics.FromImage(resizedCanvas))
+            {
+                g.DrawImage(canvas, 0, 0, this.ClientSize.Width, this.ClientSize.Height);
+            }
+
+            this.Invalidate();
+
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                isMouseDown = true;
+                lastPoint = e.Location;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown && isDrawing)
+            {
+              
+                using (Graphics g = Graphics.FromImage(canvas))
+                {
+                    using (Pen pen = new Pen(selectedColor, brushSize))
+                    {
+                        g.DrawLine(pen, lastPoint, e.Location);
+                    }
+                }
+
+                
+                this.Invalidate();
+                lastPoint = e.Location;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            selectedColor = Color.Red;
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            selectedColor = Color.Green;
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            selectedColor = Color.Blue;
+        }
+
+        private void btnBlack_Click(object sender, EventArgs e)
+        {
+            selectedColor = Color.Black;
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            selectedColor = Color.Yellow;
+        }
+
+        private void btnPurple_Click(object sender, EventArgs e)
+        {
+            selectedColor = Color.Purple;
+        }
+
+        private void btnSmall_Click(object sender, EventArgs e)
+        {
+            brushSize = 2;
+        }
+
+        private void btnMedium_Click(object sender, EventArgs e)
+        {
+            brushSize = 5;
+        }
+
+        private void btnLarge_Click(object sender, EventArgs e)
+        {
+            brushSize = 10;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            
+            using (Graphics g = Graphics.FromImage(canvas))
+            {
+                g.Clear(Color.White);  
+            }
+            this.Invalidate();
+        }
+
+        private void btnErase_Click(object sender, EventArgs e)
+        {
+            selectedColor = Color.White;
+        }
+
+        
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "JPEG Image|*.jpg";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    canvas.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+            }
+        }
+
+       
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            
+            e.Graphics.DrawImage(canvas, 0, 0);
+        }
+
+        private void btnBucket_Click(object sender, EventArgs e)
+        {            
+            Color selectedColorForBucket = selectedColor;            
+            using (Graphics g = Graphics.FromImage(canvas))
+            {
+                g.Clear(selectedColorForBucket); 
+            }           
+            this.Invalidate();
+        }
+
+    }
+}
